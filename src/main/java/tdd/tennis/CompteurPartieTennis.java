302cc0e65eb53a42c78e595be63845b96cee2b6d
Fyfeu Feuzme <feuzme@gmail.com>
2021-06-17 16:13:53 +0200

    test(score) 30 a 40 ok

--- src/main/java/tdd/tennis/CompteurPartieTennis.java
+++ src/main/java/tdd/tennis/CompteurPartieTennis.java
@@ -1,15 +1,18 @@
 package tdd.tennis;
+
 /**
- * Cette classe contient tout le code permettant de calculer les points au tennis.
+ * Cette classe contient tout le code permettant de calculer les points au
+ * tennis.
+ * 
  * @author Killian
  *
  */
 public class CompteurPartieTennis {
 
 	/**
-	 * Cette Methode permet de créer une nouvelle partie de tennis.
-	 * Une partie contient 2 joueurs et 2 Scores
-	 * Au debut de la partie, les score sont a 0.
+	 * Cette Methode permet de créer une nouvelle partie de tennis. Une partie
+	 * contient 2 joueurs et 2 Scores Au debut de la partie, les score sont a 0.
+	 * 
 	 * @param joueur1 premier joueur de la partie
 	 * @param joueur2 deuxieme joueur de la partie
 	 * @return une nouvelle partie.
@@ -17,23 +20,39 @@ public class CompteurPartieTennis {
 	public PartieDeTennis nouvellePartie(JoueurDeTennis joueur1, JoueurDeTennis joueur2) {
 		return new PartieDeTennis(joueur1, joueur2);
 	}
-	
+
 	/**
 	 * Cette methode doit etre appeler lors ce qu'un joueur gagne un point.
-	 * @param partie qui est jouer.
+	 * 
+	 * @param partie  qui est jouer.
 	 * @param gagnant Joueur qui a gagner.
 	 * @return La partie avec les nouveaux scores.
 	 */
 	public PartieDeTennis joueurGagne(PartieDeTennis partie, JoueurDeTennis gagnant) {
 		ScoreTennis nvScoreTennis = new ScoreTennis();
 		
-		nvScoreTennis.setPoints(15);
-		
-		System.out.println(gagnant + " " + partie.getScoreJoueur1());
-		if(gagnant == partie.getJoueur1()) 
-			partie.setScoreJoueur1(nvScoreTennis);		
-		if(gagnant == partie.getJoueur2())
+		if (gagnant == partie.getJoueur1()) {
+			if (partie.getScoreJoueur1().getPoints() == 0)
+				nvScoreTennis.setPoints(15);
+			if (partie.getScoreJoueur1().getPoints() == 15)
+				nvScoreTennis.setPoints(30);
+			if (partie.getScoreJoueur1().getPoints() == 30)
+				nvScoreTennis.setPoints(40);
+			
+			partie.setScoreJoueur1(nvScoreTennis);
+		}
+
+		if (gagnant == partie.getJoueur2()) {			
+			if(partie.getScoreJoueur2().getPoints() == 0)
+				nvScoreTennis.setPoints(15);			
+			if(partie.getScoreJoueur2().getPoints() == 15)
+				nvScoreTennis.setPoints(30);			
+			if(partie.getScoreJoueur2().getPoints() == 30)
+				nvScoreTennis.setPoints(40);			
+			System.out.println(nvScoreTennis.getPoints());
 			partie.setScoreJoueur2(nvScoreTennis);
+		}
+
 		return partie;
 	}
 }
--- src/test/java/tdd/tennis/CompteurPartieTennisTest.java
+++ src/test/java/tdd/tennis/CompteurPartieTennisTest.java
@@ -22,75 +22,97 @@ class CompteurPartieTennisTest {
 		partie = compteur.nouvellePartie(joueur1, joueur2);
 		assertNotNull(partie);
 	}
-	
+
 	@Test
 	@DisplayName("Au début de la partie le joueur 1 a 0 points.")
 	void test_joueur1_score_debut_nul() {
 		assertEquals(0, partie.getScoreJoueur1().getPoints());
 	}
-	
+
 	@Test
 	@DisplayName("Au début de la partie le joueur 2 a 0 points.")
 	void test_joueur2_score_debut_nul() {
 		assertEquals(0, partie.getScoreJoueur2().getPoints());
 	}
-	
+
 	@Test
 	@DisplayName("Au début de la partie les joueurs ont 0 jeux gagnés.")
 	void test_joueurs_jeux_debut_nuls() {
 		assertEquals(0, partie.getScoreJoueur2().getJeux());
 		assertEquals(0, partie.getScoreJoueur1().getJeux());
 	}
-	
+
 	@Test
 	@DisplayName("Au début de la partie les joueurs ont 0 sets gagnés.")
 	void test_joueurs_sets_debut_nuls() {
 		assertEquals(0, partie.getScoreJoueur2().getSet());
 		assertEquals(0, partie.getScoreJoueur1().getSet());
 	}
-	
+
 	@Test
 	@DisplayName("Au début de la partie les joueurs ont 0 matchs gagnés.")
 	void test_joueurs_matchs_debut_nuls() {
 		assertEquals(0, partie.getScoreJoueur2().getSet());
 		assertEquals(0, partie.getScoreJoueur1().getSet());
 	}
-	
+
 	@Test
 	@DisplayName("L'utilisateur peut notifier que Joueur1 a gagné un point.")
 	void test_joueur1_gagme_point() {
 		partie = compteur.joueurGagne(partie, partie.getJoueur1());
 		assertNotEquals(0, partie.getScoreJoueur1().getPoints());
 	}
-	
+
 	@Test
 	@DisplayName("L'utilisateur peut notifier que Joueur2 a gagné un point.")
 	void test_joueur2_gagme_point() {
 		partie = compteur.joueurGagne(partie, partie.getJoueur2());
 		assertNotEquals(0, partie.getScoreJoueur2().getPoints());
 	}
-	
+
 	@Test
-	@DisplayName("Quand un joueur gagne un point, il passe de 0 à 15")
-	void test_joueur_gagme_0a15() {
-		assertEquals(0, partie.getScoreJoueur1().getPoints());
+	@DisplayName("Quand joueur1 gagne un point, il passe de 0 à 15")
+	void test_joueur1_gagme_0a15() {
 		partie = compteur.joueurGagne(partie, partie.getJoueur1());
 		assertEquals(15, partie.getScoreJoueur1().getPoints());
 	}
+
 	@Test
-	@DisplayName("Quand un joueur gagne un point, il passe de 15 à 30")
-	void test_joueur_gagme_15a30() {
-		assertEquals(15, partie.getScoreJoueur1().getPoints());
+	@DisplayName("Quand joueur2 gagne un point, il passe de 0 à 15")
+	void test_joueur2_gagme_0a15() {
+		partie = compteur.joueurGagne(partie, partie.getJoueur2());
+		assertEquals(15, partie.getScoreJoueur2().getPoints());
+	}
+
+	@Test
+	@DisplayName("Quand un joueur1 gagne un point, il passe de 15 à 30")
+	void test_joueur1_gagme_15a30() {
+		partie.getScoreJoueur1().setPoints(15);
 		partie = compteur.joueurGagne(partie, partie.getJoueur1());
 		assertEquals(30, partie.getScoreJoueur1().getPoints());
 	}
+
 	@Test
-	@DisplayName("Quand un joueur gagne un point, il passe de 30 à 40.")
-	void test_joueur_gagme_30a40() {
-		assertEquals(30, partie.getScoreJoueur1().getPoints());
+	@DisplayName("Quand un joueur2 gagne un point, il passe de 15 à 30")
+	void test_joueur2_gagme_15a30() {
+		partie.getScoreJoueur2().setPoints(15);
+		partie = compteur.joueurGagne(partie, partie.getJoueur2());
+		assertEquals(30, partie.getScoreJoueur2().getPoints());
+	}
+
+	@Test
+	@DisplayName("Quand un joueur1 gagne un point, il passe de 30 à 40.")
+	void test_joueur1_gagme_30a40() {
+		partie.getScoreJoueur1().setPoints(30);
 		partie = compteur.joueurGagne(partie, partie.getJoueur1());
 		assertEquals(40, partie.getScoreJoueur1().getPoints());
 	}
-	
-	
+
+	@Test
+	@DisplayName("Quand un joueur2 gagne un point, il passe de 30 à 40.")
+	void test_joueur2_gagme_30a40() {
+		partie.getScoreJoueur2().setPoints(30);
+		partie = compteur.joueurGagne(partie, partie.getJoueur2());
+		assertEquals(40, partie.getScoreJoueur2().getPoints());
+	}
 }
